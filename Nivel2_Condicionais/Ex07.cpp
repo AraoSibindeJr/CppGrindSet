4cbc13bf3114b1426599adf34cb1b56b9dbeb23a
AraoSibindeJr <araosibinde098junior@gmail.com>
2025-04-23 10:14:38 +0200

    feat: Exercise07 Done

--- Nivel2_Condicionais/Ex07.cpp
+++ Nivel2_Condicionais/Ex07.cpp
@@ -1,17 +1,24 @@
-//  7. Solicite um número de  e classifique-o em faixas de valor.
+/*Enunciado da Questão
+  Crie um programa que solicite ao usuário a entrada de um número real. Em seguida, classifique esse número nas seguintes faixas de valor:
+  Menor que 0: "Número negativo"
+  Entre 0 (inclusive) e 10 (exclusive): "Número pequeno"
+  Entre 10 (inclusive) e 100 (exclusive): "Número médio"
+  Maior ou igual a 100: "Número grande"
+  O programa deve exibir a mensagem correspondente à faixa em que o número se encontra.*/
 #include <iostream>
 #include <stdio.h>
 using namespace std;
 main(){
-  int num;
-  cout<<"Insira um numero:";
+  double num;
+  cout<<"Insira um numero real qualquer:";
   cin>>num;
-  if (num > 10){
-    cout<<"Valor Dezena";
-  }else if (num > 100){
-    cout<<"Valor Centena";
+  if (num < 0){
+    cout<<"Numero negativo";
+  }else if (num <= 10){
+    cout<<"Numero pequeno";
+  }else if( num <= 100){
+    cout<<"Numero medio";
+  }else{
+    cout<<"Numero grande";
   }
-  
-  
-
 }
