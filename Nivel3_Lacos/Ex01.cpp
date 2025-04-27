bc5b6223b3a19366ab57a45134f83b9166582e6d
AraoSibindeJr <araosibinde098junior@gmail.com>
2025-04-27 11:38:35 +0200

    feat: Bug fixed Ex01

---
+++ Nivel3_Lacos/Ex01.cpp
@@ -0,0 +1,12 @@
+// 1. Leia 10 números e armazene-os em um vetor.
+# include <iostream>
+# include <stdio.h>
+using namespace std;
+main(){
+  int v [5]; // Criacao do vector
+  int cont = sizeof(v);
+  for (int i = 0; i < sizeof(v); i++){
+    /* code */
+    cout<<endl<<"ola";
+  }
+}
