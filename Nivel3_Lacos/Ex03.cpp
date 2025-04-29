ae8becc004a2a85e053e8f53798bc6fdef05f8ee
AraoSibindeJr <araosibinde098junior@gmail.com>
2025-04-29 14:43:09 +0200

    feat(Nivel03_Ex03): Added new functionality

--- Nivel3_Lacos/Ex03.cpp
+++ Nivel3_Lacos/Ex03.cpp
@@ -7,6 +7,14 @@ main(){
   cout<<endl<< " => SOMADOR DE 1 ATE N <=  ";
   cout<<endl<< " Insira o ultimo elemento da soma: ";
   cin>>n;
+  while (n < 0){
+    cout<<endl<<"ERRO";
+    cout<<endl<<" Insira apenas numeros positivos!";
+    cout<<endl<<" Insira o ultimo numero da soma: ";
+    cout<<endl<<"NB: Valor Positivo:  ";
+    cin>>n;
+  }
+
   for(int i = 1; i <= n; i++){
     sum = sum + i;
     if( i == n ){  // Condicao para omitir o sinal "+" no ultimo loop
