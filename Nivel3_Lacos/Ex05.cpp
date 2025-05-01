801a756501316ffb092952a6fadca17bcd36e549
AraoSibindeJr <araosibinde098junior@gmail.com>
2025-05-01 10:06:36 +0200

    feat(Nivel03_Ex05):Funcionalities added.

--- Nivel3_Lacos/Ex05.cpp
+++ Nivel3_Lacos/Ex05.cpp
@@ -10,15 +10,23 @@ main(){
   double fat = 1, num;
   cout<<endl<<"Insira um numero: ";
   cin>> num;
-  cout<<endl<<"Fatorial de: " << num;
-  cout<<endl<<"";
-  for(int i =num; i > 0; i--){
-    fat = fat * i;
-    if (i != 1){
-      cout<< i <<" * ";  
-    }else{
-      cout<<i<<" = ?? ";
+  if(num>0){ //Factorial de Numero Positivo
+    for(int i =num; i > 0; i--){
+      fat = fat * i;
+      if (i != 1){
+        cout<< i <<" * ";  
+      }else{
+        cout<<i<<" = ?? ";
+      }
     }
+    // Saida de Dados:
+    cout<<endl<<"Fatorial de: " << num;
+    cout<<endl<<" RESULTADO: "<< num <<"! = "<<fat;
+  }else if (num < 0){ //Factorial de Numero Negativo
+    cout<<endl<<"Funcionalidade Indisponivel no momento.";
+  }else{ //Factorial de Zero
+    fat = 0;
+    cout<<endl<<"Fatorial de: " << num;
+    cout<<endl<<" RESULTADO: "<< num <<"! = "<<fat;
   }
-  cout<<endl<<" RESULTADO: "<< num <<"! = "<<fat;
 }
