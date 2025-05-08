d9be303a2cf67810a94a5f1285be064c63dd95ed
AraoSibindeJr <araosibinde098junior@gmail.com>
2025-05-08 08:26:46 +0200

    feat(Nivel03): Exercise 10 Done.

---
+++ Nivel3_Lacos/Ex10.cpp
@@ -0,0 +1,17 @@
+//  10. Leia números até que o usuário digite 0 e calcule a soma total
+#include<iostream>
+#include<stdio.h>
+using namespace std;
+main(){
+  double totSum, num, cont = 1;
+  cout<<endl<< "Somador de Numeros: ";
+  cout<<endl<< "NOTA: Digita [O] para sair do loop.";
+  do{
+    cout<<endl<<"Insira o " << cont << " numero: ";
+    cin>>num;
+    totSum = totSum + num;
+    cont++;
+  } while (num != 0);
+  
+  cout<<endl<<" A Soma dos Numeros: " << totSum;
+}
