f408cc02d65b1819e72fe7e9da866d71e17804fd
AraoSibindeJr <araosibinde098junior@gmail.com>
2025-05-09 07:50:21 +0200

    feat(Nivel04): Exercise 01 in progress.

--- Nivel4_Vetores/Ex01.cpp
+++ Nivel4_Vetores/Ex01.cpp
@@ -3,5 +3,13 @@
 #include<stdio.h>
 using namespace std;
 main(){
-  
+  int v[3];
+  for(int i =1; i <= sizeof(v) / sizeof(v[0]); i++){
+    cout<<endl<< " Insira um numero: ";
+    cin>>v[i];
+  }
+  cout<<endl<<" Valores armazenados no vector: ";
+  for(int i =1; i <= sizeof(v) / sizeof(v[0]); i++){
+    cout<<endl<<v[i];
+  }
 }
