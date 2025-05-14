bfa115a702a32b6973a05721d32b1087d82bcbb0
AraoSibindeJr <araosibinde098junior@gmail.com>
2025-05-14 08:53:41 +0200

    feat(Nivel04): Exercise 05 Done.

--- Nivel4_Vetores/Ex05.cpp
+++ Nivel4_Vetores/Ex05.cpp
@@ -1,25 +1,26 @@
-//  5. Inverta a ordem dos elementos de um vetor de 10 posições.
+//  5. Inverta a ordem dos elementos de um vetor que o usuario vai inserir o tamanho.
 #include<iostream>
 #include<stdio.h>
 using namespace std;
 main(){
- int v[3];
+ int tamanho;
+ cout<<"Insira o Tamanho do vetor: "<<endl;
+ cin>>tamanho;
+ int v[tamanho];
  cout<<"Preencher o vector: "<<endl;
  // Preencher vetor
- for(int i =0; i<3; i++){
+ for(int i =0; i<tamanho; i++){
   cout<<"Insira um numero: "<<endl;
   cin>>v[i];
  } 
  // Mostrar valores antes da mudanca de posicoes
- cout<<"Valores na Ordem de Insercao"<<endl;
- for(int i =0; i<3; i++){
-  cout<< v[i]<<endl;
- }
+    cout<<"Valores na Ordem de Insercao"<<endl;
+    for(int i =0; i<tamanho; i++){
+     cout<< v[i]<<endl;
+   }
  // Mudar a ordem dos valores (Permutacao dos Indices)
- cout<<"Valores na mudanca de ordem"<<endl;
- for(int i =2; i>=0; i--){
-   cout<< v[i]<<endl;
- } 
- // Mostrar as mudancas de posicao com mais detalhes
- 
+    cout<<"Valores na mudanca de ordem"<<endl;
+    for(int i =tamanho-1; i>=0; i--){
+      cout<< v[i]<<endl;
+    }  
 }
